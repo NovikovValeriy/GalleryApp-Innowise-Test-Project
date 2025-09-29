907b1edadf42f6491fc574f62a75b896fba44507
NovikovValeriy <novikov.valera2005@gmail.com>
2025-09-29 19:19:11 +0300

    [ADDED] Added PhotoRepository protocol to domain layer

---
+++ GalleryApp-Innowise/Domain/Repositories/PhotoRepository.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoRepository.swift
+//  GalleryApp-Innowise
+//
+//  Created by Валерий Новиков on 29.09.25.
+//
+
+import Foundation
+
+protocol PhotoRepository {
+    func getCuratedPhotos(page: Int, perPage: Int, completion: @escaping (Result<[Photo], Error>) -> Void)
+    func downloadPhoto(url: String, completion: @escaping (Result<Data, Error>) -> Void)
+}
