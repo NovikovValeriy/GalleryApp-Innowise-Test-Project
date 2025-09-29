90fd21de900f498465dd727b84cc87347174e58a
NovikovValeriy <novikov.valera2005@gmail.com>
2025-09-29 18:43:55 +0300

    [FEATURE] Added view controller for saved photos

---
+++ GalleryApp-Innowise/Presentation/Scenes/SavedPhotos/SavedPhotosViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SavedPhotosViewController.swift
+//  GalleryApp-Innowise
+//
+//  Created by Валерий Новиков on 29.09.25.
+//
+
+import UIKit
+
+class SavedPhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+}
