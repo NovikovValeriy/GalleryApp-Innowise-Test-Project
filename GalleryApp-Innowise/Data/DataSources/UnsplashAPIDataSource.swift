bea8dc2414557400183aab64ec5f22df268509a6
NovikovValeriy <novikov.valera2005@gmail.com>
2025-09-29 20:33:10 +0300

    [ADDED] Implemented UnsplashAPIDataSource fetching and downloading photos methods

--- GalleryApp-Innowise/Data/DataSources/UnsplashAPIDataSource.swift
+++ GalleryApp-Innowise/Data/DataSources/UnsplashAPIDataSource.swift
@@ -5,6 +5,98 @@
 //  Created by Валерий Новиков on 29.09.25.
 //
 
+import Foundation
+
 class UnsplashAPIDataSource {
     
+    private let session: URLSession = {
+        let sessionConfig = URLSessionConfiguration.default
+        return URLSession(configuration: sessionConfig)
+    }()
+    
+    private var components: URLComponents = {
+        var comp = URLComponents()
+        comp.scheme = "https"
+        comp.host = "api.unsplash.com"
+        return comp
+    }()
+    
+    private func makeURLRequest(url: URL) -> URLRequest {
+        var request = URLRequest(url: url)
+        let key = "Client-ID \(UnsplashApiKey.key.rawValue)"
+        request.setValue(key, forHTTPHeaderField: "Authorization")
+        return request
+    }
+    
+    func fetchFeedPhotos(page: Int, perPage: Int, completion: @escaping (Result<[PhotoDTO], Error>) -> Void) {
+        var comp = self.components
+        comp.path = "/photos"
+        comp.queryItems = [
+            URLQueryItem(name: "page", value: "\(page)"),
+            URLQueryItem(name: "per_page", value: "\(perPage)")
+        ]
+        
+        guard let url = comp.url else {
+            completion(.failure(NSError()));
+            return
+        }
+        let req = self.makeURLRequest(url: url)
+        
+        
+        let task = self.session.dataTask(with: req) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NSError()))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError()))
+                return
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode([PhotoDTO].self, from: data)
+                completion(.success(decodedData))
+            } catch {
+                completion(.failure(NSError()))
+            }
+        }
+        task.resume()
+    }
+    
+    func downloadPhoto(url: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let url = URL(string: url) else {
+            completion(.failure(NSError()))
+            return
+        }
+        
+        let task = self.session.downloadTask(with: url) { localURL, response, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NSError()))
+                return
+            }
+            
+            guard let localURL = localURL else {
+                completion(.failure(NSError()))
+                return
+            }
+            
+            do {
+                let data = try Data(contentsOf: localURL)
+                completion(.success(data))
+            } catch {
+                completion(.failure(NSError()))
+            }
+        }
+        task.resume()
+    }
 }
---
+++ GalleryApp-Innowise/Data/DataSources/UnsplashApiKey.swift
@@ -0,0 +1,10 @@
+//
+//  UnsplashApiKey.swift
+//  GalleryApp-Innowise
+//
+//  Created by Валерий Новиков on 29.09.25.
+//
+
+enum UnsplashApiKey: String {
+    case key = "0O0KB0mKsop8NJXOtBCpwfZHmPjGcTxqVmm6NMf-hys"
+}
