221deda2637d9c6749165129eb7aa0c05fe2254e
NovikovValeriy <novikov.valera2005@gmail.com>
2025-10-01 16:07:30 +0300

    [ADDED] Addded collection view and its respective cell to feed photos controller

--- GalleryApp-Innowise/Presentation/Scenes/FeedPhotos/FeedPhotosViewController.swift
+++ GalleryApp-Innowise/Presentation/Scenes/FeedPhotos/FeedPhotosViewController.swift
@@ -6,12 +6,45 @@
 //
 
 import UIKit
+import CHTCollectionViewWaterfallLayout
 
 class FeedPhotosViewController: UIViewController {
 
+    // MARK: - UI elements
+    
+    private let collectionView: UICollectionView = {
+        let layout = CHTCollectionViewWaterfallLayout()
+        layout.itemRenderDirection = .shortestFirst
+        layout.minimumColumnSpacing = 0
+        layout.minimumInteritemSpacing = 2
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(PhotosWaterfallCollectionViewCell.self, forCellWithReuseIdentifier: PhotosWaterfallCollectionViewCell.identifier)
+        collectionView.showsVerticalScrollIndicator = false
+        return collectionView
+    }()
+    
+    // MARK: - UI Configuration
+    
+    private func collectionViewConfiguration() {
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    private func configureUI() {
+        self.collectionViewConfiguration()
+    }
+    
+    // MARK: - ViewController lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        self.configureUI()
     }
 
 }
---
+++ GalleryApp-Innowise/Presentation/Views/PhotosWaterfallCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosWaterfallCollectionViewCell.swift
+//  GalleryApp-Innowise
+//
+//  Created by Валерий Новиков on 1.10.25.
+//
+
+import UIKit
+
+class PhotosWaterfallCollectionViewCell: UICollectionViewCell {
+    static let identifier = "PhotosWaterfallCollectionViewCell"
+}
