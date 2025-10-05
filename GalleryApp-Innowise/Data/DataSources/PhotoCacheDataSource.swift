018795197366e59e885e8b84c03a2d43c7c10906
NovikovValeriy <novikov.valera2005@gmail.com>
2025-10-05 19:03:32 +0300

    [ADDED] Added photo cache data source implementation

--- GalleryApp-Innowise/Data/DataSources/PhotoCacheDataSource.swift
+++ GalleryApp-Innowise/Data/DataSources/PhotoCacheDataSource.swift
@@ -5,6 +5,17 @@
 //  Created by Валерий Новиков on 29.09.25.
 //
 
+import Foundation
+
 class PhotoCacheDataSource {
+    private let imageCache: NSCache<NSString, NSData> = NSCache<NSString, NSData>()
     
+    func getImageData(for url: String) -> Data? {
+        let key = url as NSString
+        return imageCache.object(forKey: key) as? Data
+    }
+    
+    func setImageData(data: Data, for url: String) {
+        imageCache.setObject(data as NSData, forKey: url as NSString)
+    }
 }
