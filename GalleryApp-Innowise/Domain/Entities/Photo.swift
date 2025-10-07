76e7daff1ff9ffd9a46ec07a5d36b5de18f58f42
NovikovValeriy <novikov.valera2005@gmail.com>
2025-10-08 02:07:48 +0300

    [REFACTOR] Changed Photo domain model field types from nullable

--- GalleryApp-Innowise/Domain/Entities/Photo.swift
+++ GalleryApp-Innowise/Domain/Entities/Photo.swift
@@ -7,15 +7,15 @@
 
 struct Photo: Hashable {
     let id: String
-    let width: Int?
-    let height: Int?
-    let averageColor: String?
+    let width: Int
+    let height: Int
+    let averageColor: String
     let descriptionText: String?
     let altDescription: String?
-    let thumbnailUrl: String?
-    let fullUrl: String?
-    let authorName: String?
-    let authorUsername: String?
+    let thumbnailUrl: String
+    let fullUrl: String
+    let authorName: String
+    let authorUsername: String
     
     static func == (lhs: Photo, rhs: Photo) -> Bool {
         lhs.id == rhs.id
