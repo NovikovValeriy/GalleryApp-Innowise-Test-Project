42fb65c4e3691e48921db506ef74c10b5ab17906
NovikovValeriy <novikov.valera2005@gmail.com>
2025-10-07 11:36:24 +0300

    [ADDED] Added GetSavedPhotoUseCase

---
+++ GalleryApp-Innowise/Domain/UseCases/GetSavedPhotosUseCase.swift
@@ -0,0 +1,21 @@
+//
+//  GetSavedPhotosUseCase.swift
+//  GalleryApp-Innowise
+//
+//  Created by Валерий Новиков on 7.10.25.
+//
+
+protocol GetSavedPhotosUseCase: AnyObject {
+    func execute(completion: @escaping (Result<[Photo], Error>) -> Void)
+}
+
+class GetSavedPhotosUseCaseImpl: GetSavedPhotosUseCase {
+    private let repository: SavedPhotoRepository
+    init(repository: SavedPhotoRepository) {
+        self.repository = repository
+    }
+
+    func execute(completion: @escaping (Result<[Photo], Error>) -> Void) {
+        repository.fetchSavedPhotos(completion: completion)
+    }
+}
