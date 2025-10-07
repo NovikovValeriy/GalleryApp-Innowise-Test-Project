3fe3c57ed71da0069db1ff137a62ea988fe8f668
NovikovValeriy <novikov.valera2005@gmail.com>
2025-10-07 14:14:14 +0300

    [ADDED] Added SavedPhotosViewModel and its implementation

---
+++ GalleryApp-Innowise/Presentation/Scenes/SavedPhotos/SavedPhotosViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  SavedPhotosViewModel.swift
+//  GalleryApp-Innowise
+//
+//  Created by Валерий Новиков on 7.10.25.
+//
+
+import Foundation
+
+protocol SavedPhotosViewModel: AnyObject {
+    var photos: [Photo] { get }
+    
+    var onSavedPhotosUpdated: (() -> Void)? { get set }
+    var onPhotoPressed: ((Int) -> Void)? { get set }
+    
+    func getSavedPhotos()
+}
+
+class SavedPhotosViewModelImpl: SavedPhotosViewModel {
+    private let getSavedPhotosUseCase: GetSavedPhotosUseCase
+    
+    private(set) var photos: [Photo] = []
+    
+    var onSavedPhotosUpdated: (() -> Void)?
+    
+    var onPhotoPressed: ((Int) -> Void)?
+    
+    init(getSavedPhotosUseCase: GetSavedPhotosUseCase) {
+        self.getSavedPhotosUseCase = getSavedPhotosUseCase
+    }
+    
+    func getSavedPhotos() {
+        getSavedPhotosUseCase.execute { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let savedPhotos):
+                self.photos.append(contentsOf: savedPhotos)
+                self.onSavedPhotosUpdated?()
+            case .failure(_):
+                return
+            }
+        }
+    }
+}
