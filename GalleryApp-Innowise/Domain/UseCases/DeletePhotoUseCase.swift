2ec2d53567ce8eedb434b54ef33813d34568f068
NovikovValeriy <novikov.valera2005@gmail.com>
2025-10-07 11:34:56 +0300

    [ADDED] Added DeletePhotoUseCase

---
+++ GalleryApp-Innowise/Domain/UseCases/DeletePhotoUseCase.swift
@@ -0,0 +1,21 @@
+//
+//  DeletePhotoUseCase.swift
+//  GalleryApp-Innowise
+//
+//  Created by Валерий Новиков on 7.10.25.
+//
+
+protocol DeletePhotoUseCase: AnyObject {
+    func execute(id: String, completion: @escaping (Result<Void, Error>) -> Void)
+}
+
+class DeletePhotoUseCaseImpl: DeletePhotoUseCase {
+    private let repository: SavedPhotoRepository
+    init(repository: SavedPhotoRepository) {
+        self.repository = repository
+    }
+
+    func execute(id: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        repository.deletePhoto(with: id, completion: completion)
+    }
+}
